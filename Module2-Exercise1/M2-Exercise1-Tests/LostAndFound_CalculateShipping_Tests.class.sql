d3574037fe47acfbf4140ca3edd5131aaa09694b
Paul Kardash <paul.kardash@aimco.ca>
2021-04-18 20:25:57 -0600

    Added schema and a function and a test and as many tests I could think of

--- Module2-Exercise1/M2-Exercise1-Tests/LostAndFound_CalculateShipping_Tests.class.sql
+++ Module2-Exercise1/M2-Exercise1-Tests/LostAndFound_CalculateShipping_Tests.class.sql
@@ -4,6 +4,42 @@ GO
 CREATE PROCEDURE LostAndFound_CalculateShipping_Tests.[test something]
 AS
 BEGIN
-  EXEC tSQLt.Fail 'TODO';
+	DECLARE @ExpectedShippingCost INTEGER;
+	DECLARE @ActualShippingCost INTEGER;
+
+	SELECT @ActualShippingCost = LostAndFound.CalculateShipping('FL','FL');
+
+	SET @ExpectedShippingCost = 10;
+	EXEC tSQLt.AssertEquals @ExpectedShippingCost, @ActualShippingCost;
+
+	SELECT @ActualShippingCost = LostAndFound.CalculateShipping('FL','NY');
+
+	SET @ExpectedShippingCost = 25;
+	EXEC tSQLt.AssertEquals @ExpectedShippingCost, @ActualShippingCost;
+
+	SELECT @ActualShippingCost = LostAndFound.CalculateShipping(NULL,'NY');
+
+	SET @ExpectedShippingCost = NULL;
+	EXEC tSQLt.AssertEquals @ExpectedShippingCost, @ActualShippingCost;
+
+	SELECT @ActualShippingCost = LostAndFound.CalculateShipping('FL',NULL);
+
+	SET @ExpectedShippingCost = NULL;
+	EXEC tSQLt.AssertEquals @ExpectedShippingCost, @ActualShippingCost;
+
+	SELECT @ActualShippingCost = LostAndFound.CalculateShipping(NULL,NULL);
+
+	SET @ExpectedShippingCost = NULL;
+	EXEC tSQLt.AssertEquals @ExpectedShippingCost, @ActualShippingCost;
+
+	SELECT @ActualShippingCost = LostAndFound.CalculateShipping('','');
+
+	SET @ExpectedShippingCost = NULL;
+	EXEC tSQLt.AssertEquals @ExpectedShippingCost, @ActualShippingCost;
+
+	SELECT @ActualShippingCost = LostAndFound.CalculateShipping(' ','');
+
+	SET @ExpectedShippingCost = NULL;
+	EXEC tSQLt.AssertEquals @ExpectedShippingCost, @ActualShippingCost;
 END
 GO
