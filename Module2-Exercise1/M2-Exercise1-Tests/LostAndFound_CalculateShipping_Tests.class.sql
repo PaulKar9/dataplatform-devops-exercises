adf3c6597f583199558241f4037e3c88a4f8a182
Paul Kardash <paul.kardash@aimco.ca>
2021-04-21 16:29:40 -0600

    refactored the one test into seperated tests

--- Module2-Exercise1/M2-Exercise1-Tests/LostAndFound_CalculateShipping_Tests.class.sql
+++ Module2-Exercise1/M2-Exercise1-Tests/LostAndFound_CalculateShipping_Tests.class.sql
@@ -1,7 +1,7 @@
 EXEC tSQLt.NewTestClass 'LostAndFound_CalculateShipping_Tests';
 GO
 
-CREATE PROCEDURE LostAndFound_CalculateShipping_Tests.[test different permutation of different input parameters]
+CREATE PROCEDURE LostAndFound_CalculateShipping_Tests.[test same states]
 AS
 BEGIN
 	DECLARE @ExpectedShippingCost INTEGER;
@@ -12,30 +12,83 @@ BEGIN
 	SET @ExpectedShippingCost = 10;
 	EXEC tSQLt.AssertEquals @ExpectedShippingCost, @ActualShippingCost;
 
+END
+GO
+
+CREATE PROCEDURE LostAndFound_CalculateShipping_Tests.[test different states]
+AS
+BEGIN
+	DECLARE @ExpectedShippingCost INTEGER;
+	DECLARE @ActualShippingCost INTEGER;
+
 	SELECT @ActualShippingCost = LostAndFound.CalculateShipping('FL','NY');
 
 	SET @ExpectedShippingCost = 25;
 	EXEC tSQLt.AssertEquals @ExpectedShippingCost, @ActualShippingCost;
+END
+GO
+
+CREATE PROCEDURE LostAndFound_CalculateShipping_Tests.[test NULL Hotel state]
+AS
+BEGIN
+	DECLARE @ExpectedShippingCost INTEGER;
+	DECLARE @ActualShippingCost INTEGER;
 
 	SELECT @ActualShippingCost = LostAndFound.CalculateShipping(NULL,'NY');
 
 	SET @ExpectedShippingCost = NULL;
 	EXEC tSQLt.AssertEquals @ExpectedShippingCost, @ActualShippingCost;
 
+END
+GO
+
+CREATE PROCEDURE LostAndFound_CalculateShipping_Tests.[test NULL Shipping state]
+AS
+BEGIN
+	DECLARE @ExpectedShippingCost INTEGER;
+	DECLARE @ActualShippingCost INTEGER;
+
 	SELECT @ActualShippingCost = LostAndFound.CalculateShipping('FL',NULL);
 
 	SET @ExpectedShippingCost = NULL;
 	EXEC tSQLt.AssertEquals @ExpectedShippingCost, @ActualShippingCost;
+
+END
+GO
+
+CREATE PROCEDURE LostAndFound_CalculateShipping_Tests.[test NULL states]
+AS
+BEGIN
+	DECLARE @ExpectedShippingCost INTEGER;
+	DECLARE @ActualShippingCost INTEGER;
 
 	SELECT @ActualShippingCost = LostAndFound.CalculateShipping(NULL,NULL);
 
 	SET @ExpectedShippingCost = NULL;
 	EXEC tSQLt.AssertEquals @ExpectedShippingCost, @ActualShippingCost;
 
+END
+GO
+
+CREATE PROCEDURE LostAndFound_CalculateShipping_Tests.[test empty strings states]
+AS
+BEGIN
+	DECLARE @ExpectedShippingCost INTEGER;
+	DECLARE @ActualShippingCost INTEGER;
+
 	SELECT @ActualShippingCost = LostAndFound.CalculateShipping('','');
 
 	SET @ExpectedShippingCost = NULL;
 	EXEC tSQLt.AssertEquals @ExpectedShippingCost, @ActualShippingCost;
+
+END
+GO
+
+CREATE PROCEDURE LostAndFound_CalculateShipping_Tests.[test white space and empty states]
+AS
+BEGIN
+	DECLARE @ExpectedShippingCost INTEGER;
+	DECLARE @ActualShippingCost INTEGER;
 
 	SELECT @ActualShippingCost = LostAndFound.CalculateShipping(' ','');
 
