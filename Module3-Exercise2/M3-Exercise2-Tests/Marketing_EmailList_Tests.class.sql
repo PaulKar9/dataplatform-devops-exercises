73962f87cbcf4ea407d5a90021f5231ad568fd85
Paul Kardash <paul.kardash@aimco.ca>
2021-04-26 13:53:09 -0600

    added number of reservations to the view and some tests to cover that - fixed tests that broke as a result :)

--- Module3-Exercise2/M3-Exercise2-Tests/Marketing_EmailList_Tests.class.sql
+++ Module3-Exercise2/M3-Exercise2-Tests/Marketing_EmailList_Tests.class.sql
@@ -46,13 +46,13 @@ BEGIN
 	INTO ActualEmailList
 	FROM Marketing.EmailList
 
-	SELECT TOP 0 *
+	SELECT TOP 0 CustomerId, FirstName, LastName, Email, OptIn
 	INTO ExpectedEmailList
 	FROM Marketing.EmailList
 
 	INSERT INTO ExpectedEmailList (CustomerId, FirstName, LastName, Email, OptIn)
-						   VALUES (12, 'Oksana', 'Kardash','okardash@gmail.com',1)
-						         ,(11, 'Paul', 'Kardash', 'pkardash@gmail.com', 1)
+						   VALUES (11, 'Paul', 'Kardash', 'pkardash@gmail.com', 1),
+								  (12, 'Oksana', 'Kardash','okardash@gmail.com',1);
 
 	EXEC tSQLt.AssertEqualsTable ExpectedEmailList, ActualEmailList;
 
@@ -60,7 +60,7 @@ BEGIN
 END
 GO
 
-CREATE PROCEDURE Marketing_EmailList_Tests.[test no one opted in]
+CREATE PROCEDURE Marketing_EmailList_Tests.[test no one opted in - two customers]
 AS
 BEGIN
 	IF OBJECT_ID(N'ActualEmailList') IS NOT NULL DROP TABLE ActualEmailList;
@@ -76,5 +76,52 @@ BEGIN
 	FROM Marketing.EmailList
 
 	EXEC tSQLt.AssertEmptyTable ActualEmailList;
+END
+GO
+
+CREATE PROCEDURE Marketing_EmailList_Tests.[test no one opted in - no customers]
+AS
+BEGIN
+	IF OBJECT_ID(N'ActualEmailList') IS NOT NULL DROP TABLE ActualEmailList;
+
+	EXEC tSQLt.FakeTable 'Booking.Customers';
+
+	SELECT CustomerId, FirstName, LastName, Email, OptIn
+	INTO ActualEmailList
+	FROM Marketing.EmailList
+
+	EXEC tSQLt.AssertEmptyTable ActualEmailList;
+END
+GO
+
+CREATE PROCEDURE Marketing_EmailList_Tests.[test more tan 1 reservations per customer]
+AS
+BEGIN
+	IF OBJECT_ID(N'ActualEmailList') IS NOT NULL DROP TABLE ActualEmailList;
+	IF OBJECT_ID(N'ExpectedEmailList') IS NOT NULL DROP TABLE ExpectedEmailList;
+
+	EXEC tSQLt.FakeTable 'Booking.Customers';
+	EXEC tSQLt.FakeTable 'Booking.Reservations'
+
+	INSERT INTO Booking.Customers (CustomerId, FirstName, LastName, Email, OptIn)
+						   VALUES (11, 'Paul', 'Kardash', 'pkardash@gmail.com', 1);
+
+	INSERT INTO Booking.Reservations (ReservationId, CustomerId)
+						      VALUES (1, 11), (2, 11), (3, 11)
+
+	SELECT CustomerId, FirstName, LastName, Email, OptIn, CountReservations
+	INTO ActualEmailList
+	FROM Marketing.EmailList
+
+	SELECT 
+		11 AS CustomerId, 
+		'Paul' AS FirstName, 
+		'Kardash' AS LastName, 
+		'pkardash@gmail.com' AS Email, 
+		1 AS OptIn,
+		3 AS CountReservations
+	INTO ExpectedEmailList
+
+	EXEC tSQLt.AssertEqualsTable ExpectedEmailList, ActualEmailList;
 END
 GO
--- Module3-Exercise2/M3-Exercise2/Marketing.EmailList.view.sql
+++ Module3-Exercise2/M3-Exercise2/Marketing.EmailList.view.sql
@@ -1,4 +1,18 @@
 CREATE VIEW Marketing.EmailList AS
-SELECT CustomerId, FirstName, LastName, Email, OptIn
-FROM Booking.Customers
+SELECT 
+	BC.CustomerId, 
+	BC.FirstName, 
+	BC.LastName, 
+	BC.Email, 
+	BC.OptIn,
+	COUNT(BR.ReservationId) AS CountReservations
+FROM Booking.Customers BC
+LEFT JOIN Booking.Reservations BR 
+	ON BC.CustomerId = BR.CustomerId
 WHERE OptIn = 1
+GROUP BY
+	BC.CustomerId, 
+	BC.FirstName, 
+	BC.LastName, 
+	BC.Email, 
+	BC.OptIn
